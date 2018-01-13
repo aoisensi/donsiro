d3f847c842fa5e10618b59c15918147b23a2b42d
aoisensi <aoisensi@outlook.jp>
2018-01-13 19:12:36 +0900

    pref

--- app/src/main/java/info/aoisensi/donsiro/Donsiro.kt
+++ app/src/main/java/info/aoisensi/donsiro/Donsiro.kt
@@ -1,10 +1,18 @@
 package info.aoisensi.donsiro
 
 import android.app.Application
+import android.preference.PreferenceManager
 
 /**
  * Created by aoisensi on 2018/01/03.
  *
  */
 
-class Donsiro : Application()
+class Donsiro : Application() {
+    fun addAccount(accessToken: String, domain: String) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val accounts = pref.getStringSet("accounts", HashSet())
+        //TODO accounts.add()
+        pref.edit().putStringSet("accounts", accounts).apply()
+    }
+}
